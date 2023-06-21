cbccc61c361744c83a569166a9f480ce272e15db
rizkyizh <rizkiizzulhaq14@gmail.com>
2023-06-21 21:16:39 +0700

    models

---
+++ src/main/java/com/registration/course/clientapp/models/Member.java
@@ -0,0 +1,23 @@
+package com.registration.course.clientapp.models;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+
+  private Integer id;
+  private String name;
+  private String email;
+  private Long phone;
+  private String city;
+  private String nationaly;
+  private User user;
+  List<Transaction> transactions;
+
+}
