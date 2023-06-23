4b1702bced10c696c8340f25f0ca8b984f54261a
rizkyizh <rizkiizzulhaq14@gmail.com>
2023-06-23 16:00:02 +0700

    sign out button and route

--- src/main/java/com/registration/course/clientapp/controllers/auth/LoginController.java
+++ src/main/java/com/registration/course/clientapp/controllers/auth/LoginController.java
@@ -1,8 +1,10 @@
 package com.registration.course.clientapp.controllers.auth;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,6 +48,12 @@ public class LoginController {
       return "redirect:/login";
     }
     return "redirect:/";
+  }
+
+  @GetMapping("/logout")
+  public String logout() {
+    SecurityContextHolder.clearContext();
+    return "redirect:/login";
   }
 
 }
